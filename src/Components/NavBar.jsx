548ed93a8b4b8125bf66b9eda62480d5f60208ff
royfuyi <91306040+royfuyi@users.noreply.github.com>
2025-08-28 16:39:54 -0700

    minor formatting and scroll behaviour edits

--- src/Components/NavBar.jsx
+++ src/Components/NavBar.jsx
@@ -3,12 +3,15 @@ import { Button } from "./Button"
 import './NavBar.css'
 
 export function NavBar() {
+    const handleReload = () => {
+        window.scrollTo(0, 0)
+      };
     return (
         <>
             <div className="nav">
-                <Link to="/illustrations"><Button className="navButton" text="illustrations" size="5vw"/></Link>
-                <Link to="/"><div className="homeButton"/></Link>
-                <Link to="/commissioninfo"><Button className="navButton" text="commission info" size="5vw"/></Link>
+                <Link to="/illustrations" onClick={handleReload}><Button className="navButton" text="illustrations" size="5vw"/></Link>
+                <Link to="/" onClick={handleReload}><div className="homeButton"/></Link>
+                <Link to="/commissioninfo" onClick={handleReload}><Button className="navButton" text="commission info" size="5vw"/></Link>
             </div>
         </>
     )
--- src/Pages/PortfolioPage.jsx
+++ src/Pages/PortfolioPage.jsx
@@ -1,6 +1,10 @@
+import { useEffect } from "react"
 import { Images } from "../Components/Images.jsx"
 
 export function PortfolioPage() {
+    useEffect(()=>{
+        window.scrollTo(0, 0)
+    },[]);
     return (
         <>
         <p className="introText">here are some examples of my work !</p>
